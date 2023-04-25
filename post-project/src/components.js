88f934ff5228cd0b12b58cee28220cb84a1efe03
Mark Chigrin <mark@ilmpc.xyz>
2023-04-25 14:00:52 +0300

    Add markup

---
+++ post-project/src/components.js
@@ -0,0 +1,37 @@
+export function userPageComponent({ user, posts }) {
+  return `
+    <div class="">
+      ${userComponent(user)}
+      <hr class="m-4 border-gray-300">
+      <div class="grid md:grid-cols-2 justify-center">
+        ${posts.map(postComponent).join("")}
+      </div>
+    </div>
+  `;
+}
+
+export function userComponent(user) {
+  return `
+    <div class="m-2 p-5 rounded-md shadow-lg flex gap-4 items-center">
+      <img class="rounded-full" src="https://placehold.co/100">
+      <div class="text-left">
+        <h2 class="font-bold">${user.name}</h2>
+        <h4 class="text-slate-600">${user.email}</h4>
+        <span class="text-slate-500">Website:</span> <a href="https://${user.website}">${user.website}</a>
+      </div>
+    </div>
+  `;
+}
+
+export function postComponent(post) {
+  return `
+    <div class="m-2 p-7 rounded-md shadow-lg max-w-md">
+      <h3 class="text-lg font-bold mb-3">${post.title}</h3>
+      <p class="text-left">${post.body}</p>
+    </div>
+  `;
+}
+
+export function loadingComponent() {
+  return `<div class="lds-grid m-10"><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div></div>`;
+}
--- post-project/src/main.js
+++ post-project/src/main.js
@@ -1,34 +1,13 @@
 import "./style.css";
 import { getUserWithPosts } from "./api.js";
+import { userPageComponent, loadingComponent } from "./components.js";
 
 const root = document.querySelector("#app");
 
-root.innerHTML = `Loading...`;
-
-getUserWithPosts(5).then((data) => {
+async function render() {
+  root.innerHTML = loadingComponent();
+  const data = await getUserWithPosts(5);
   root.innerHTML = userPageComponent(data);
-});
-
-function userPageComponent({ user, posts }) {
-  return `
-    <div class="">
-      <div>
-        <h2>${user.name}</h2>
-        <h4>${user.email}</h4>
-      </div>
-      <h2>Posts:</h2>
-      <div>
-        ${posts.map(postComponent).join("")}
-      </div>
-    </div>
-  `;
 }
 
-function postComponent(post) {
-  return `
-    <div class="m-2 p-5 rounded-md shadow-lg max-w-sm">
-      <h3 class="text-lg font-bold mb-3">${post.title}</h3>
-      <p class="text-left">${post.body}</p>
-    </div>
-  `;
-}
+render();
